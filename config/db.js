3b9aeb2ac8fb3299d3c6ddb1df6df29d4d52778e
Miguel Jerone Ormita <mjormita@indracompany.com>
2025-09-02 04:23:34 +0800

    fix-deployment error

--- config/db.js
+++ config/db.js
@@ -1,16 +1,22 @@
-require("dotenv").config();
+// require("dotenv").config();
 const mongoose = require("mongoose");
+
+const MONGODB_URI =
+  "mongodb+srv://sample123:sample123@phfloodwatch.aknndmt.mongodb.net/ecom-platform?retryWrites=true&w=majority&appName=PHFloodWatch";
 
 // Connect to MongoDB Atlas
 const connectDB = (starServer) => {
-  return mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => {
-      console.log("✅ Connected to MongoDB Atlas");
-      // Start Server
-      starServer();
-    })
-    .catch((err) => console.log("❌ MongoDB Error:", err));
+  return (
+    mongoose
+      // .connect(process.env.MONGODB_URI)
+      .connect(MONGODB_URI)
+      .then(() => {
+        console.log("✅ Connected to MongoDB Atlas");
+        // Start Server
+        starServer();
+      })
+      .catch((err) => console.log("❌ MongoDB Error:", err))
+  );
 };
 
 module.exports = connectDB;
